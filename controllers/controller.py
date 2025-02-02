76edf3f9dd440f5439781859d487304f9945f62b
David Kneringer Foss <david.k.foss@gmail.com>
2025-02-02 13:39:00 +0100

    Classic PID controller implemented

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,2 +1,16 @@
+import numpy as np
+
+
 class Controller:
-    pass
+    def __init__(self, params):
+        self.errors = []
+        self.learning_rate = self.params["learning_rate"]
+
+    def update_params(self, params, grads):
+        """Updates the parameters based on the new error (E)."""
+        raise NotImplementedError("Subclass must implement update_params()")
+
+    def control_signal(self, error):
+        """Calculates and returns the control signal (U)."""
+        raise NotImplementedError(
+            "Subclass must implement get_control_signal()")
--- controllers/pid.py
+++ controllers/pid.py
@@ -2,4 +2,29 @@ from .controller import Controller
 
 
 class PIDController(Controller):
-    pass
+    def __init__(self, params):
+        super().__init__(params)
+        self.kp = 0.5
+        self.ki = 0.1
+        self.kd = 0.05
+
+    def update_params(self, params, grads):
+        g_kp = grads[0]
+        g_ki = grads[2]
+        g_kd = grads[1]
+
+        self.kp -= self.learning_rate*g_kp
+        self.kd -= self.learning_rate*g_kd
+        self.ki -= self.learning_rate*g_ki
+
+    def control_signal(self, error):
+        self.errors.append(error)
+
+        P = self.kp*error
+        I = self.ki*sum(self.errors)
+        D = self.kd*(errors[-1]-errors[-2]) if len(self.errors) > 1 else 0
+
+        return P+I+D
+
+    def print_params(self):
+        print("kp: " + self.kp + ", ki: " + self.ki + ", kd: " + self.kd)
